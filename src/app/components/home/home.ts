98e18b45e45e5d408dae173c0faf968d894e5551
valdez-jose <tu-email-de-github@example.comclear>
2026-06-11 16:27:58 -0300

    Actualizacion de imagen home.css

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -14,7 +14,8 @@ export class HomeComponent implements OnInit {
     
     'https://images.homify.com/v1500404452/p/photo/image/2131069/1.jpg',
     'https://nettcontrol.com/wp-content/uploads/sala-de-juntas-moderna-con-crestron-1024x695.jpg',
-    'https://static.vecteezy.com/system/resources/thumbnails/072/261/233/small/modern-office-space-with-stunning-city-view-and-natural-light-photo.jpg'
+    'https://static.vecteezy.com/system/resources/thumbnails/072/261/233/small/modern-office-space-with-stunning-city-view-and-natural-light-photo.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTua4KWwFMzWtqH7Ul9a8HNAPBgfbF4Jr60eLLNyFUqlQ&s=10'
   ];
 
   indice = 0;
