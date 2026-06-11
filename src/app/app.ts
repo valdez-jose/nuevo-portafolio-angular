6b26ef07023cc17310dda4aedd4feff4eaf76e3c
valdez-jose <tu-email-de-github@example.comclear>
2026-06-11 16:05:41 -0300

    Agregar carrusel de imágenes al portafolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,16 @@
 import { Component } from '@angular/core';
 // 👇 AGREGÁ ESTA LÍNEA (revisá que la ruta coincida con tus carpetas)
 import { Navbar } from './components/navbar/navbar'; 
-import { Home } from './components/home/home';       // Supongo que tenés estos también
+import {HomeComponent} from './components/home/home'; // Y esta también
 import { Footer } from './components/footer/footer'; // Supongo que tenés estos también
 
 @Component({
   selector: 'app-root',
   standalone: true, // Si estás usando componentes Standalone (Angular moderno)
-  imports: [Navbar, Home, Footer], // <--- Acá es donde te tiraba el error
+  imports: [Navbar, HomeComponent, Footer], // <--- Acá es donde te tiraba el error
   templateUrl: './app.html',
 })
 export class App {
   // ...
 }
+
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class HomeComponent implements OnInit {
+
+  imagenes: string[] = [
+    
+    'https://images.homify.com/v1500404452/p/photo/image/2131069/1.jpg',
+    'https://nettcontrol.com/wp-content/uploads/sala-de-juntas-moderna-con-crestron-1024x695.jpg',
+    'https://static.vecteezy.com/system/resources/thumbnails/072/261/233/small/modern-office-space-with-stunning-city-view-and-natural-light-photo.jpg'
+  ];
+
+  indice = 0;
+  imagenActual = this.imagenes[0];
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.siguiente();
+    }, 3000);
+  }
+
+  siguiente() {
+    this.indice++;
+
+    if (this.indice >= this.imagenes.length) {
+      this.indice = 0;
+    }
+
+    this.imagenActual = this.imagenes[this.indice];
+  }
+
+  anterior() {
+    this.indice--;
+
+    if (this.indice < 0) {
+      this.indice = this.imagenes.length - 1;
+    }
+
+    this.imagenActual = this.imagenes[this.indice];
+  }
+}
